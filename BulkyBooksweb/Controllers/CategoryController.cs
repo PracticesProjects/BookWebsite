addc164e06fbd83ff55b5999503175ef88d013dd
Sagar1555 <stimalsena155@gmail.com>
2022-09-29 13:47:31 +0545

    display order

--- BulkyBooksweb/Controllers/CategoryController.cs
+++ BulkyBooksweb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBooksweb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBooksweb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
